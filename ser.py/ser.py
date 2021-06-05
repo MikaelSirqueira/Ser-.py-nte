6c0b71fc3bce6911f37c56b38a48b53c848205ca
danielnowakassis <daniel.nowak.assis@gmail.com>
2021-06-04 22:32:03 -0300

    adições de múltiplos de 3

--- ser.py/ser.py
+++ ser.py/ser.py
@@ -8,6 +8,7 @@ preto=(0,0,0)
 vermelho=(255,0,0)
 verde=(0,255,0)
 azul=(0,0,255)
+amarelo = (255,255,0)
 
 try:
     pygame.init()
@@ -23,27 +24,8 @@ relogio = pygame.time.Clock()
 fundo = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Ser.pynte")
 font = pygame.font.SysFont(None, 15)
-
-def perguntas(n):
-    if n == 1:
-        print("oi1")
-    if n == 2:
-        print("oi2")
-    if n == 3:                
-        print("oi3")
-    if n == 4:
-        print("oi4")
-    if n == 5:
-        print("oi5")    
-    if n == 6:
-        print("oi6")    
-    if n == 7:
-        print("oi7")
-    if n == 8:
-        print("oi8")
-    if n == 9:
-        print("oi9")
-
+corcobra = preto
+corfundo = branco
 
 
 
@@ -52,11 +34,33 @@ def texto(msg, cor,tam, x, y):
     font = pygame.font.SysFont(None, tam)
     texto1 = font.render(msg, True, cor)
     fundo.blit(texto1, [x,y])
+def perguntas(n):
+    if n == 1:
+        print("oi")
+        texto("1- Python é uma linguagem: ", vermelho, 25, 55, 30)
+        pygame.draw.rect(fundo, preto, [45, 120, 135 ,27])
+        texto("Interpretada", branco, 30, 50 ,125)
+        pygame.draw.rect(fundo, preto, [190, 120, 120, 27])
+        texto("Compilada", branco, 30, 195 ,125)
+        # for event in pygame.event.get():
+        #     if event.type == pygame.KEYDOWN:
+        #          if event.key == pygame.K_1:
+        #             break
+    # if n == 2:
+    if n == 3:
+        corcobra = amarelo
+    # if n == 4:
+    # if n == 5:
+    # if n == 6:
+    # if n == 7:
+    # if n == 8:
+    # if n == 9:
+
 
 #função da cobra
 def cobra(CobraXY):
     for XY in CobraXY:
-        pygame.draw.rect(fundo, preto, [XY[0], XY[1], tamanho, tamanho])
+        pygame.draw.rect(fundo, corcobra, [XY[0], XY[1], tamanho, tamanho])
 
 #função da maça
 def maca(pos_x, pos_y):
@@ -74,7 +78,9 @@ def jogo():
     CobraXY = []
     CobraComp = 1
     pontos = 0
-    pergunta = 0
+    pergunta = 1
+    despausar = True
+    telamorte = True
     while sair:
         while fimdejogo:
             for event in pygame.event.get():
@@ -94,9 +100,14 @@ def jogo():
                         CobraXY = []
                         CobraComp = 1
                         pontos = 0
+                        pergunta = 0
                     if event.key == pygame.K_s:
                         sair = False
                         fimdejogo = False
+                    if pontos == 3:
+                        if event.type == pygame.K_1:
+                            sair = False
+
                 #Mouse
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x = pygame.mouse.get_pos()[0]
@@ -113,18 +124,19 @@ def jogo():
                         CobraXY = []
                         CobraComp = 1
                         pontos = 0
+                        pergunta = 0
                     elif x > 190 and y > 120 and x < 265 and y < 147:
                         sair = False
                         fimdejogo = False
-
-            fundo.fill(branco)
-            texto("Fim de jogo", vermelho, 50, 65, 30)
-            texto("Pontuação Final: " +str(pontos), preto, 30, 70, 80)
-            pygame.draw.rect(fundo, preto, [45, 120, 135 ,27])
-            texto("Continuar(C)", branco, 30, 50 ,125)
-            pygame.draw.rect(fundo, preto, [190, 120, 75, 27])
-            texto("Sair(S))", branco, 30, 195 ,125)
-            pygame.display.update()
+            if telamorte:
+                fundo.fill(corfundo)
+                texto("Fim de jogo", vermelho, 50, 65, 30)
+                texto("Pontuação Final: " +str(pontos), preto, 30, 70, 80)
+                pygame.draw.rect(fundo, preto, [45, 120, 135 ,27])
+                texto("Continuar(C)", branco, 30, 50 ,125)
+                pygame.draw.rect(fundo, preto, [190, 120, 75, 27])
+                texto("Sair(S))", branco, 30, 195 ,125)
+                pygame.display.update()
         #movimentação cobra
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -144,7 +156,7 @@ def jogo():
                     velocidade_y=tamanho
         #questão estetica do código para o fim de jogo
         if sair:
-            fundo.fill(branco)
+            fundo.fill(corfundo)
             pos_x+=velocidade_x
             pos_y+=velocidade_y
 
@@ -155,37 +167,46 @@ def jogo():
                 maca_y=randrange(0,altura-tamanho - 40,10)
                 CobraComp += 1
                 pontos += 1
-                if pontos % 3 == 0:
-                    pergunta += 1
-                    perguntas(pergunta)
-            #Regras de parede 
-            if pos_x + tamanho > largura:
-                pos_x = 0
-            if pos_x < 0:
-                pos_x=largura-tamanho
-            if pos_y + tamanho> altura - placar:
-                pos_y = 0
-            if pos_y < 0:
-                pos_y= altura-tamanho - placar
+                if pontos == 3:
+                    fimdejogo = True
+                    sair = True
+                    telamorte = False
+                # if pontos % 3 == 0:
+                    # pergunta += 1
+                    # despausar = False
+                    # perguntas(pergunta)
+                    # pygame.display.update()
+                # else:
+                    # despausar = True
                 
-            CobraInicio = []
-            CobraInicio.append(pos_x)
-            CobraInicio.append(pos_y)
-            CobraXY.append(CobraInicio)
-            if len(CobraXY) > CobraComp:
-                del CobraXY[0]
-            #fim de jogo, colisão da cobra com sí própria
-            if any(Bloco == CobraInicio for Bloco in CobraXY[:-1]):
-                fimdejogo = True
+            if despausar:
+                #Regras de parede 
+                if pos_x + tamanho > largura:
+                    pos_x = 0
+                if pos_x < 0:
+                    pos_x=largura-tamanho
+                if pos_y + tamanho> altura - placar:
+                    pos_y = 0
+                if pos_y < 0:
+                    pos_y= altura-tamanho - placar
+                    
+                CobraInicio = []
+                CobraInicio.append(pos_x)
+                CobraInicio.append(pos_y)
+                CobraXY.append(CobraInicio)
+                if len(CobraXY) > CobraComp:
+                    del CobraXY[0]
+                #fim de jogo, colisão da cobra com sí própria
+                if any(Bloco == CobraInicio for Bloco in CobraXY[:-1]):
+                    fimdejogo = True
+                    telamorte = True
 
-            pygame.draw.rect(fundo, preto, [0, altura-placar, largura, placar])
-            texto("Pontuação: "+str(pontos), branco, 20, 10, altura - 30)
-            cobra(CobraXY)
-            maca(maca_x,maca_y)
-            pygame.display.update()
-            relogio.tick(20)
+                pygame.draw.rect(fundo, preto, [0, altura-placar, largura, placar])
+                texto("Pontuação: "+str(pontos), branco, 20, 10, altura - 30)
+                cobra(CobraXY)
+                maca(maca_x,maca_y)
+                pygame.display.update()
+                relogio.tick(20)
 
-
-    
 jogo()
 pygame.quit()
